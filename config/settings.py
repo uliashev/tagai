efcc939a98bd2c7b4ad7394ebf1164ae250a856c
Alexey Ulyashev <alexeyulyashev@gmail.com>
2025-12-09 11:56:36 +0200

    allowed hosts modified

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ DEBUG = settings.DEBUG
 # Render provides RENDER_EXTERNAL_HOSTNAME
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tagai.onrender.com']
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 if DEBUG:
